03fcb58ced721f0d5952b757b2a75d83565b43d1
Guilherme Andrade <g@gandrade.net>
2018-11-25 16:00:41 +0000

    Make room for other Common Test suites

--- test/algorithmic_SUITE.erl
+++ test/algorithmic_SUITE.erl
@@ -18,7 +18,7 @@
 %% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 %% DEALINGS IN THE SOFTWARE.
 
--module(aequitas_SUITE).
+-module(algorithmic_SUITE).
 -compile(export_all).
 
 -include_lib("eunit/include/eunit.hrl").
